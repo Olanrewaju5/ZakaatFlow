b4d179a76f3ea57305981e7abc4be9fd3fab35a4
Mac <mac@example.com>
2026-03-23 12:48:13 +0100

    added cash to the logic

--- src/store/useZakatStore.js
+++ src/store/useZakatStore.js
@@ -12,7 +12,8 @@ const initialZakatState = {
   
   goldSilver: {
     goldValue: 0,
-    silverValue: 0,
+    silverValue: 0, 
+    cashOnHand: 0, // Cash value of gold/silver jewelry
     jewelryUsageType: 'personal_use', // 'personal_use' | 'investment'
   },
   
